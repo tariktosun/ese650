1602cfe069ced36b9395e548177258d67da92b89
Tarik <tarik@modlab-vision-2.(none)>
2014-03-21 16:49:21 -0400

    test_data passes for all datasets (basic test)

--- 4/test_a_priori.m
+++ 4/test_a_priori.m
@@ -3,6 +3,25 @@ classdef test_a_priori < matlab.unittest.TestCase
     %   Detailed explanation goes here
     
     properties
+        raw_data
+        data
+    end
+    
+    methods (TestMethodSetup)
+        function setup(testCase)
+            addpath(genpath('data'));
+            % load data:
+            load Encoders20.mat
+            load Hokuyo20.mat
+            imu = load('imuRaw20.mat');
+            raw_data = struct();
+            raw_data.Hokuyo = Hokuyo0;
+            raw_data.Encoders = Encoders;
+            raw_data.imu = imu;
+            testCase.raw_data = raw_data;
+            % clean the data:
+            testCase.data = clean_data( testCase.raw_data );
+        end
     end
     
     methods (Test)
--- 4/test_data.m
+++ 4/test_data.m
@@ -4,26 +4,33 @@ classdef test_data < matlab.unittest.TestCase
     
     properties
         raw_data
+        names
     end
     
     methods (TestMethodSetup)
         function setup(testCase)
             addpath(genpath('data'));
             % load data:
-            load Encoders20.mat
-            load Hokuyo20.mat
-            imu = load('imuRaw20.mat');
-            raw_data = struct();
-            raw_data.Hokuyo = Hokuyo0;
-            raw_data.Encoders = Encoders;
-            raw_data.imu = imu;
-            testCase.raw_data = raw_data;
+            testCase.names = {'20', '21', '22', '23', '24'};
+            raw_data = {};
+            for i=1:numel(testCase.names)
+                load(['Encoders' testCase.names{i} '.mat']);
+                load(['Hokuyo' testCase.names{i} '.mat']);
+                imu = load(['imuRaw' testCase.names{i} '.mat']);
+                raw_data = struct();
+                raw_data.Hokuyo = Hokuyo0;
+                raw_data.Encoders = Encoders;
+                raw_data.imu = imu;
+                testCase.raw_data{i} = raw_data;
+            end
         end
     end
     
     methods (Test)
         function test_clean_data(testCase)
-            data = clean_data( testCase.raw_data );
+            for i=1:numel(testCase.names)
+                data = clean_data( testCase.raw_data{i} );
+            end
         end
         
     end
