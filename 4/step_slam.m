e79fb04a7e083a2f77592d1c258cb020b797d6f8
Tarik Tosun <tarikt@seas.upenn.edu>
2014-03-20 23:05:53 -0400

    added a_posteriori.m

--- 4/step_slam.m
+++ 4/step_slam.m
@@ -8,7 +8,7 @@ function [ output, slam_state ] = step_slam( data, p_slam_state, p_output, param
 a_priori_particles = a_priori( p_particles, data.odometry, params );
 
 %% (2) a posteriori estimate for particles and map using scan matching
-[a_posteriori_particles, a_posteriori_map] = ...
+[a_posteriori_weights, a_posteriori_map] = ...
     a_posteriori( a_priori_particles, p_output.map, params );
 
 %% (3) re-sample particles
