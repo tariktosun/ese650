690c6e23515ee4d6c607cd56d72310b7d8299dae
Tarik Tosun <tarikt@seas.upenn.edu>
2014-03-24 17:48:55 -0400

    added plot_world.m

---
+++ 4/plot_world.m
@@ -0,0 +1,9 @@
+function plot_world( pos, map, params )
+% plot_world( pos, map, params )
+% Plots the world and robot.
+imshow(map);
+hold on
+pos_cell = to_cell_indices( pos, params );
+plot( pos_cell(2), pos_cell(1), 'r*'); % flipped b/c image
+hold off
+end
