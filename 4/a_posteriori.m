f28d1f0d5f370cf117fd3d5add68668cc673e697
Tarik <tarik@modlab-vision-2.(none)>
2014-03-24 00:15:14 -0400

    linked things up a bit

--- 4/a_posteriori.m
+++ 4/a_posteriori.m
@@ -1,27 +1,29 @@
 function [a_posteriori_weights, a_posteriori_map] = ...
-    a_posteriori( a_priori_particles, p_output.map, ranges, angles, params )
+    a_posteriori( a_priori_particles, slam_state, map, data, params )
 % [a_posteriori_weights, a_posteriori_map] = ...
 %    a_posteriori( a_priori_particles, p_output.map, params )
 %
+%%
+a_priori_weights = slam_state.weights;
+a_posteriori_weights = a_priori_weights;
+NP = size(a_priori_particles,2);
+Y = cell();
+Yind = cell();
 %% Apply xforms to Hokuyo measurements, and transform to from meters to cells:
-for i=1:num_particles
+for i=1:NP
     % Apply xforms
-    Y{i} = transform_range( a_priori_particles(:,i), ranges, angles ); % {P}x3x1081 array
+    Y{i} = transform_range( a_priori_particles(:,i), data.ranges, data.angles ); % {P}x3x1081 array
     % convert to cell indices:
     Yind{i} = to_cell_indices( Y{i} ); % 1x1081 (or fewer) indices.
 end
 %% Compute a map correlation value for each particle:
-for i=1:num_particles
+for i=1:NP
     %compute correlation
     c = correlation( map, Y{i}, params );
-    a_posteriori_weights(i) = a_priori_weights .* c;
+    a_posteriori_weights(i) = a_priori_weights(i) .* c;
 end
 %% Compute a posteriori map:
-for i=1:num_particles
-    % generate rays:
-    getMapCellsFromRay();
-    % Add rays as free space:
-        % weight appropriately
-    % Add endpoints as filled space:
+for i=1:NP
+    map = write_to_map( map, to_cell_indices(a_priori_particles(:,i),params), Yind{i}, a_posteriori_weights(i), params);
 end
 end
--- 4/step_slam.m
+++ 4/step_slam.m
@@ -9,14 +9,19 @@ a_priori_particles = a_priori( slam_state, data, params );
 
 %% (2) a posteriori estimate for particles and map using scan matching
 [a_posteriori_weights, a_posteriori_map] = ...
-    a_posteriori( a_priori_particles, map, data, params );
+    a_posteriori( a_priori_particles, slam_state, map, data, params );
 
 %% (3) re-sample particles
+%{
+Neff = 
 if Neff < alpha * num_particles
     particles = resample_particles( particles, a_posteriori_weights, params );
 end
+%}
 
 %% Package output and return.
 map = a_posteriori_map;
 slam_state.particles = particles;
+slam_state.weights = a_posteriori_weights;
 slam_state.time = data.ts;
+pos = slam_state.particles * slam_state.weights';   % weighted average
