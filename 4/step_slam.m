7f4754f71e62308e2f85c450e190fc7d5ca67a62
Tarik Tosun <tarikt@seas.upenn.edu>
2014-03-25 09:50:29 -0400

    added images and test

--- 4/step_slam.m
+++ 4/step_slam.m
@@ -1,24 +1,16 @@
 function [pos, map, slam_state] = step_slam( data, slam_state, map, params )
-%[ output, slam_state ] = step_slam( data, p_slam_state, p_output )
 %
-%% initialization
-particles = slam_state.particles;
-
 %% (1) a priori estimate for particles using odometry
 a_priori_particles = a_priori( slam_state, data, params );
 slam_state.particles = a_priori_particles;
-
 %% (2) a posteriori estimate for particles and map using scan matching
 [a_posteriori_weights, a_posteriori_map] = a_posteriori( slam_state, map, data, params );
-
 %% (3) re-sample particles
 %{
-Neff = 
 if Neff < alpha * num_particles
     particles = resample_particles( particles, a_posteriori_weights, params );
 end
 %}
-
 %% Package output and return.
 map = a_posteriori_map;
 slam_state.weights = a_posteriori_weights;
