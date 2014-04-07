fda5ad6a7410c66d7a9df56202ea94167c76750c
Tarik <tarik@modlab-vision-2.(none)>
2014-04-07 15:40:31 -0400

    added index_image.m

--- 5/matlab/sandbox.m
+++ 5/matlab/sandbox.m
@@ -1,25 +1,8 @@
-%% Test basic planning:
+A = feature_map;
+i1 = [1,3,5]';
+i2 = [1,3,5]';
+%i3 = repmat([1:5], numel(i1), 1);
+i3 = [1:5]
+%i1 + (i2-1)*size(A,1) + (i3-1)*size(A,1)*size(A,2)
+idx = bsxfun(@plus, i1, bsxfun(@plus, (i2-1)*size(A,1), (i3-1)*size(A,1)*size(A,2)) )
 
-costs = C;
-
-goal = [250 1000];
-tic;
-ctg = dijkstra_matrix(costs,goal(1),goal(2));
-toc
-
-[ip1, jp1] = dijkstra_path(ctg, costs, 1, 1);
-[ip2, jp2] = dijkstra_path2(ctg, costs, 1, 1);
-
-subplot(1,2,1);
-imagesc(costs,[1 10]);
-colormap(1-gray);
-hold on;
-plot(jp1, ip1, 'b-', jp2, ip2, 'r-');
-hold off;
-
-subplot(1,2,2);
-imagesc(ctg);
-colormap(1-gray);
-hold on;
-plot(jp1, ip1, 'b-', jp2, ip2, 'r-');
-hold off;
